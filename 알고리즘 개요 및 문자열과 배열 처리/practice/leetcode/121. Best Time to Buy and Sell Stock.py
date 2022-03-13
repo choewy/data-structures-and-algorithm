e42d858e68b1483033d29c6497076efcd6920d67
choewy <choewy32@gmail.com>
2022-03-14 00:38:09 +0900

    #2 알고리즘 개요 및 문자열과 배열 처리 문제

---
+++ 알고리즘 개요 및 문자열과 배열 처리/practice/leetcode/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,61 @@
+from pprint import pprint
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # 반복문으로 해야함
+        # 이유? 아래 테스트케이스 첫 번째와 같이
+        # 사려는 값을 최소값으로 구하는 경우
+        # 찾을 수 없는 상횡이 발생할 수 있음
+
+        buy = min(prices)
+        buy_index = prices.index(buy)
+
+        prices = prices[buy_index:]
+        sell = max(prices)
+        sell_index = prices.index(sell) + buy_index
+
+        if buy_index < sell_index:
+            return sell - buy
+
+        return 0
+
+
+if __name__ == "__main__":
+    test_case = [
+        {
+            "input": [2, 4, 1],
+            "expected": 2
+        },
+        {
+            "input": [2, 1, 4],
+            "expected": 3
+        },
+        {
+            "input": [7, 1, 5, 3, 6, 4],
+            "expected": 5
+        },
+        {
+            "input": [7, 6, 4, 3, 1],
+            "expected": 0
+        },
+    ]
+
+    def testing():
+        solution = Solution()
+        for case in test_case:
+            i = case["input"]
+            o = solution.maxProfit(i)
+            e = case["expected"]
+            if o != e:
+                return {
+                    "result": False,
+                    "input": i,
+                    "output": o,
+                    "expected": e
+                }
+
+        return {"result": True}
+
+    pprint(testing())
