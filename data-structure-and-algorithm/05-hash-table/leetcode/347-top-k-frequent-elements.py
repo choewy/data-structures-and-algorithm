045827d03f354fda6d4ead432a4aa4d2457ce96d
choewy <choewy32@gmail.com>
2022-03-16 20:35:00 +0900

    data-structure-and-algorithm

--- data-structure-and-algorithm/05-hash-table/leetcode/347-top-k-frequent-elements.py
+++ data-structure-and-algorithm/05-hash-table/leetcode/347-top-k-frequent-elements.py
@@ -3,9 +3,7 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        items = {n: nums.count(n) for n in set(nums)}.items()
-        items = sorted(items, key=lambda x: (-x[1], x[0]))[:k]
-        return [x[0] for x in items]
+        return [x[0] for x in sorted({n: nums.count(n) for n in set(nums)}.items(), key=lambda x: (-x[1], x[0]))[:k]]
 
 
 if __name__ == "__main__":
