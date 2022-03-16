b4c65314ddf12111a21d0a1c5c3b7d436668e17e
choewy <choewy32@gmail.com>
2022-03-16 20:26:26 +0900

    data-structure-and-algorithm

--- data-structure-and-algorithm/05-hash-table/baekjoon/01920-find-number.py
+++ data-structure-and-algorithm/05-hash-table/baekjoon/01920-find-number.py
@@ -2,6 +2,5 @@ input()
 A = set(input().split())
 
 input()
-M = input().split()
 
-print('\n'.join(["1" if m in A else "0" for m in M]))
+print('\n'.join(["1" if m in A else "0" for m in input().split()]))
