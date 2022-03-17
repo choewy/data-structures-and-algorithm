15e937e800931cfb9cfddc6bf93e82bd817897f8
choewy <choewy32@gmail.com>
2022-03-17 12:08:34 +0900

    data-structure-and-algorithm

--- data-structure-and-algorithm/00-coding-test/42583-다리를 지나는 트럭.py
+++ data-structure-and-algorithm/00-coding-test/42583-다리를 지나는 트럭.py
@@ -1,20 +1,31 @@
 def solution(bridge_length: int, bridge_weight: int, trucks: list):
+    # Queue로 사용하기 위해 역순(내림차순 아님) 정렬
     trucks.reverse()
 
+    # 소요 시간
     time = 0
+
+    # 다리를 건너고 있는 트럭(예외처리를 위해 0으로 입력함)
     crossed = [0]*bridge_length
+
+    # 현재 다리 위 트럭의 총 무게
     weights = 0
 
     while trucks:
+        # 시간 증가 및 현재 다리 위 트럭 1개 제거(건넌 상태)
         time += 1
         weights -= crossed.pop()
 
+        # 다리 위에 트럭 한 대가 더 올라가도 버틸 수 있는지?
         if trucks[-1] + weights <= bridge_weight:
+            # 다리 위에 트럭 추가(Queue로 사용하기 위해 역순 정렬)
             crossed = [trucks.pop()] + crossed
             weights += crossed[0]
         else:
+            # Queue로 사용하기 위해 역순 정렬
             crossed = [0] + crossed
 
+    # 총 소요 시간 = 소요 시간 + 다리 길이
     return time + bridge_length
 
 
--- data-structure-and-algorithm/00-coding-test/42586-기능개발.py
+++ data-structure-and-algorithm/00-coding-test/42586-기능개발.py
@@ -1,23 +1,30 @@
-from math import ceil
-
-
 def solution(progresses, speeds):
     counts = []
 
-    days = [ceil((100-p)/s) for p, s in zip(progresses, speeds)]
+    # 진행률을 기반으로 며칠 소요될 것인지 계산
+    days = [((100-p)//s)+1 if (100-p)/s > (100-p)//s else (100-p)//s
+            for p, s in zip(progresses, speeds)]
+
+    # Queue로 사용하기 위하여 역순(내림차순 아님) 정렬
     days.reverse()
 
     while days:
+        # 배포 수 초기화
         cnt = 1
+
+        # 기능 배포 대기
         current = days.pop()
+
         while days:
-            day = days.pop()
-            if day > current:
-                days.append(day)
+            # 다음 기능 배포 대기
+            next = days.pop()
+
+            # 다음 기능의 완료일이 이전 기능의 완료일보다 늦는 경우
+            if next > current:
+                days.append(next)
                 break
+
+            # 다음 기능의 완료일이 이전 기능의 완료일보다 빠르거나 같은 경우
             cnt += 1
         counts.append(cnt)
     return counts
-
-
-solution([93, 30, 55], [1, 30, 5])
