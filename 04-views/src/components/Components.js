cbd522b3fee2df3093d9cc3b7182a2c2e25276cc
choewy <choewy32@gmail.com>
2022-04-03 18:30:42 +0900

    카드 구현을 위한 데이터 추가

--- 04-views/src/components/Components.js
+++ 04-views/src/components/Components.js
@@ -1,7 +1,7 @@
 import BreadthFirstSearch from "./contents/BreadthFirstSearch";
 import HomeIcon from '@mui/icons-material/Home';
 import GridOnIcon from '@mui/icons-material/GridOn';
-import HomePage from "./contents/HomePage";
+import HomePage from "./homepage/HomePage";
 
 const useComponents = () => {
     return [
@@ -10,14 +10,18 @@ const useComponents = () => {
             to: '/',
             label: 'HOME',
             element: <HomePage />,
-            icon: <HomeIcon />
+            icon: <HomeIcon />,
+            title: ''
         },
         {
             path: '/1',
             to: '/1',
             label: 'BFS',
             element: <BreadthFirstSearch />,
-            icon: <GridOnIcon />
+            icon: <GridOnIcon />,
+            title: 'BFS 알고리즘 구현',
+            description: '<백준 7576번 토마토>문제를 풀고, BFS 알고리즘의 동작 방식을 구현한 컴포넌트',
+            href: 'https://www.acmicpc.net/problem/7576'
         }
     ];
 };
