dbe6e5a8868e1b11f9a507a034ed778bca27360d
choewy <choewy32@gmail.com>
2022-04-03 15:39:01 +0900

    BFS 탐색 페이지 설정

--- 04-views/src/App.js
+++ 04-views/src/App.js
@@ -1,8 +1,25 @@
+import withStyles from "@mui/styles/withStyles";
+import { Route, Routes } from "react-router-dom";
+import PageIndex from "./components/commons/PageIndex";
+import BreadthFirstSearch from "./components/pages/BreadthFirstSearch";
+
+const styles = (theme) => ({
+  app: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto'
+  },
+});
+
 const App = () => {
   return (
     <div className="app">
+      <PageIndex />
+      <Routes>
+        <Route path="/1" element={<BreadthFirstSearch />} />
+      </Routes>
     </div>
   );
 };
 
-export default App;
+export default withStyles(styles)(App);
