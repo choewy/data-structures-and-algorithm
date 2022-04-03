b9225c15095e2543e00f2e247e48728806428750
choewy <choewy32@gmail.com>
2022-04-03 18:06:15 +0900

    homepage 아직 미구현

---
+++ 04-views/src/components/contents/HomePage.js
@@ -0,0 +1,16 @@
+import withStyles from "@mui/styles/withStyles";
+
+const styles = (theme) => ({
+    menuLink: {
+        textDecoration: 'none',
+        color: 'inherit'
+    }
+});
+
+const HomePage = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default withStyles(styles)(HomePage);
