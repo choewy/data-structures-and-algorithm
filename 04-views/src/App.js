c75df6bc51a16ea19f9040c9df7205670ef3ac90
choewy <choewy32@gmail.com>
2022-04-03 18:07:03 +0900

    Helmet 적용

--- 04-views/src/App.js
+++ 04-views/src/App.js
@@ -1,23 +1,66 @@
-import withStyles from "@mui/styles/withStyles";
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import PageIndex from "./components/commons/PageIndex";
-import BreadthFirstSearch from "./components/pages/BreadthFirstSearch";
+import withStyles from "@mui/styles/withStyles";
+import Box from '@mui/material/Box';
+import CssBaseline from "@mui/material/CssBaseline";
+import NavBar from "./components/commons/NavBar";
+import SideBar from "./components/commons/SideBar";
+import Paper from "@mui/material/Paper";
+import useComponents from "./components/Components";
+import Helmets from "./components/commons/Helmets";
 
 const styles = (theme) => ({
   app: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
-    overflowX: 'auto'
+    overflowX: 'hidden'
   },
+  components: {
+    marginTop: 20
+  }
 });
 
-const App = () => {
+const App = (props) => {
+  const { classes } = props;
+  const components = useComponents();
+  const [open, setOpen] = useState(false);
+
+  const navbarOpenHandler = () => {
+    setOpen(true);
+  };
+
+  const navbarCloseHandler = () => {
+    setOpen(false);
+  };
+
+  const navBarProps = {
+    open,
+    navbarOpenHandler,
+  };
+
+  const sideBarProps = {
+    open,
+    navbarCloseHandler
+  };
+
   return (
-    <div className="app">
-      <PageIndex />
-      <Routes>
-        <Route path="/1" element={<BreadthFirstSearch />} />
-      </Routes>
+    <div className={classes.app}>
+      <Helmets />
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <NavBar {...navBarProps} />
+        <SideBar {...sideBarProps} />
+      </Box>
+      <Paper className={classes.components}>
+        <Routes>
+          {
+            components.map((component, key) => {
+              const { path, element } = component;
+              const componentProps = { key, path, element };
+              return <Route {...componentProps} />;
+            })
+          }
+        </Routes>
+      </Paper>
     </div>
   );
 };
