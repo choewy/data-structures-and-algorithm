22a6064f8f9172e36fa27ce50912fb7b544bf524
choewy <choewy32@gmail.com>
2022-04-03 15:38:03 +0900

    목차 페이지

---
+++ 04-views/src/components/commons/PageIndex.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const PageIndex = () => {
+    return (
+        <ul>
+            <li><Link to='/1'>BreadthFirstSearch</Link></li>
+        </ul>
+    );
+};
+
+export default PageIndex;
